f8f0d2e70da0ec2bf1b34232e6d4b6e8e5626cf6
DiegoVP66 <dvpdev33@gmail.com>
2022-01-15 17:38:52 -0300

    Home

---
+++ src/components/Button/index.js
@@ -0,0 +1,11 @@
+import './styles.css';
+
+const Button = () => {
+  return (
+    <button className="btn-catalog">
+      <h1>VER CATÁLOGO</h1>
+    </button>
+  );
+};
+
+export default Button;
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,42 @@
+import MainImage from "assets/images/main-image.png";
+import Button from "components/Button";
+import Navbar from "components/Navbar";
+
+import "./styles.css";
+
+const Home = () => {
+  return (
+    <>
+      <Navbar />
+      <div className="home-container">
+        <div className="home-card base-card">
+          <div className="home-content-container">
+            <h1>O carro perfeito para você</h1>
+            <p>
+              Conheça nossos carros e dê mais um passo na realização do seu
+              sonho
+            </p>
+          </div>
+          <div className="home-image-container">
+            <img src={MainImage} alt="carro-principal" />
+          </div>
+        </div>
+
+        <div className="home-card-catalog-container">
+          <div className="home-card-catalog">
+            <div className="home-content-catalog">
+              <a href="link">
+                <Button />
+              </a>
+            </div>
+            <div className="home-card-link">
+              <a href="link">Comece agora a navegar</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
